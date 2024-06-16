46e3bcfec57a5e83ca49c4a11b34b993e3706a21
YOONGUNYONG <backendgun@gmail.com>
2024-06-16 11:46:03 +0900

    init commit

---
+++ src/main/java/com/example/sideMall/SideMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.sideMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideMallApplication.class, args);
+	}
+
+}
